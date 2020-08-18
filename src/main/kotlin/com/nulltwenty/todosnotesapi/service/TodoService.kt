371b1b252c14641dec8eec43234299bc9cdba2ce
Manuel Lorenzo <manulorenzop@gmail.com>
2020-08-18 15:29:00 +0200

    remove spring annotations from service

--- src/main/kotlin/com/nulltwenty/todosnotesapi/service/TodoService.kt
+++ src/main/kotlin/com/nulltwenty/todosnotesapi/service/TodoService.kt
@@ -4,15 +4,13 @@ import com.nulltwenty.todosnotesapi.data.Todo
 import com.nulltwenty.todosnotesapi.repository.TodoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
 
 @Service("Todo service")
 class TodoService {
     @Autowired
     lateinit var repository: TodoRepository
     fun getTodos(): Iterable<Todo> = repository.findAll()
-    fun insertTodo(@RequestBody todo: Todo) = repository.save(todo)
-    fun deleteTodo(@PathVariable(name = "id") id: String) = repository.deleteById(id)
-    fun updateTodo(@RequestBody todo: Todo): Todo = repository.save(todo)
+    fun insertTodo(todo: Todo) = repository.save(todo)
+    fun deleteTodo(id: String) = repository.deleteById(id)
+    fun updateTodo(todo: Todo): Todo = repository.save(todo)
 }
